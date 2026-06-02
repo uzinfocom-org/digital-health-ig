d9a3eb9687667881b9c32a196c6651eabd6b03ed
Jasur <jasurbek.az09ibrohimov@gmail.com>
2026-06-02 14:14:17 +0500

    upd

--- _genonce.sh
+++ _genonce.sh
@@ -14,42 +14,16 @@ fi
 
 echo "$txoption"
 
-echo "Running SUSHI..."
-sushi .
-if [ $? -ne 0 ]; then
-	echo "SUSHI failed. Aborting..."
-	exit 1
-fi
-
-# Remove structuredefinition-implements from generated SDs
-# (FHIR R5 core spec references this extension but it is not published in the R5 package - known upstream HL7 issue)
-python3 - <<'EOF'
-import json, glob, os
-ext_url = 'http://hl7.org/fhir/StructureDefinition/structuredefinition-implements'
-for path in glob.glob('fsh-generated/resources/StructureDefinition-*.json'):
-    with open(path) as f:
-        data = json.load(f)
-    exts = data.get('extension', [])
-    filtered = [e for e in exts if e.get('url') != ext_url]
-    if len(filtered) < len(exts):
-        data['extension'] = filtered
-        if not data['extension']:
-            del data['extension']
-        with open(path, 'w') as f:
-            json.dump(data, f, indent=2, ensure_ascii=False)
-        print(f'  Removed structuredefinition-implements from {os.path.basename(path)}')
-EOF
-
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -jar $publisher -ig . $txoption -no-sushi $*
+	java -jar $publisher -ig . $txoption $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		java -jar $publisher -ig . $txoption -no-sushi $*
+		java -jar $publisher -ig . $txoption $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
