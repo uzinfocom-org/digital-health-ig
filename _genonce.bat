1039361b97d9d2f5a4a580cf7b86723c99ec63d2
AKHMEDOV BEKHZOD <43494414+BEKHZOD98@users.noreply.github.com>
2026-06-02 18:40:00 +0500

    Merge pull request #30 from jassabr/UZCoreActivityDefinition
    Uz core activity definition

--- _genonce.bat
+++ _genonce.bat
@@ -18,11 +18,11 @@ SET txoption=
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 IF EXIST "%input_cache_path%\%publisher_jar%" (
-	JAVA -jar "%input_cache_path%\%publisher_jar%" -ig . %txoption% %*
+    JAVA -Xmx8g -jar "%input_cache_path%\%publisher_jar%" -ig . %txoption% %*
 ) ELSE If exist "..\%publisher_jar%" (
-	JAVA -jar "..\%publisher_jar%" -ig . %txoption% %*
+    JAVA -Xmx8g -jar "..\%publisher_jar%" -ig . %txoption% %*
 ) ELSE (
-	ECHO IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
+    ECHO IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 )
 
 PAUSE
